1f7b13ea27c00323256b388e07c1f4ec401431ed
Tobias Winkler <tobias.winkler@b-s-s.de>
2018-08-15 17:02:00 +0200

    Correct license header formatting

--- src/contracts/sharing.ts
+++ src/contracts/sharing.ts
@@ -10,19 +10,19 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Affero General Public License for more details.
 
-  You should have received a copy of the GNU Affero General Public License along with this program.
-  If not, see http://www.gnu.org/licenses/ or write to the
+  You should have received a copy of the GNU Affero General Public License
+  along with this program. If not, see http://www.gnu.org/licenses/ or
+  write to the Free Software Foundation, Inc., 51 Franklin Street,
+  Fifth Floor, Boston, MA, 02110-1301 USA, or download the license from
+  the following URL: https://evan.network/license/
 
-  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA,
+  You can be released from the requirements of the GNU Affero General Public
+  License by purchasing a commercial license.
+  Buying such a license is mandatory as soon as you use this software or parts
+  of it on other blockchains than evan.network.
 
-  or download the license from the following URL: https://evan.network/license/
-
-  You can be released from the requirements of the GNU Affero General Public License
-  by purchasing a commercial license.
-  Buying such a license is mandatory as soon as you use this software or parts of it
-  on other blockchains than evan.network.
-
-  For more information, please contact evan GmbH at this address: https://evan.network/license/
+  For more information, please contact evan GmbH at this address:
+  https://evan.network/license/
 */
 
 import {
