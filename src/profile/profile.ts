ed835025d4466522c633cfd83ba172ba70830429
Tobias Winkler <tobias.winkler@outlook.de>
2018-12-07 15:42:32 +0100

    profile: Remove valid contact default claim checking

--- src/profile/profile.ts
+++ src/profile/profile.ts
@@ -570,7 +570,6 @@ export class Profile extends Logger {
    */
   private async loadActiveClaims() {
     const defaultClaims = [
-      '/contacts/valid',
       '/onboarding/agbaccepted',
     ];
 
