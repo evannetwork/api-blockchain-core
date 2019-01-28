e4d50438565f3c8fadaecee32df71c62b068420f
Sebastian Dechant <sebastian.dechant@evan.team>
2019-01-28 15:24:17 +0100

    fix testutils and chain endpoint

--- src/test/test-utils.ts
+++ src/test/test-utils.ts
@@ -69,7 +69,7 @@ import { Wallet } from '../contracts/wallet';
 export const publicMailBoxExchange = 'mailboxKeyExchange';
 export const sampleContext = 'context sample';
 
-const web3Provider = 'wss://testcore.evan.network/ws';
+const web3Provider = <any>process.env.CHAIN_ENDPOINT || 'wss://testcore.evan.network/ws';
 const helperWeb3 = new Web3(null);
 const sampleKeys = {};
 // dataKeys
