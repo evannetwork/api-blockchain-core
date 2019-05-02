5ae43adb84a8f854eb2e678fd2f01f4ce03197ae
Sebastian Dechant <sebastian.dechant@evan.team>
2019-05-02 10:07:25 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,19 +19,15 @@
 
 # -- Download needed additional documentation
 import os
-#https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/
-    
-    	# retrieve required docu files from dbcp
-#	wget  -P blockchain -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/blockchain/account-store.rst"
-#	wget  -P blockchain -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/blockchain/event-hub.rst"
-#	wget  -P blockchain -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/blockchain/executor.rst"
-#	wget  -P blockchain -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/blockchain/signer.rst"
-#	wget  -P common -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/common/logger.rst"
-#	wget  -P common -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/common/validator.rst"
-#	wget  -P contracts -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/contracts/contract-loader.rst"
-#	wget  -P dfs -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/dfs/dfs-interface.rst"
-#	wget  -P dfs -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/dfs/ipfs.rst"
-#	wget  -P encryption -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/encryption/cryptor-unencrypted.rst"
+os.system('wget  -P blockchain -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/blockchain/event-hub.rst"')
+os.system('wget  -P blockchain -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/blockchain/executor.rst"')
+os.system('wget  -P blockchain -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/blockchain/signer.rst"')
+os.system('wget  -P common -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/common/logger.rst"')
+os.system('wget  -P common -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/common/validator.rst"')
+os.system('wget  -P contracts -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/contracts/contract-loader.rst"')
+os.system('wget  -P dfs -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/dfs/dfs-interface.rst"')
+os.system('wget  -P dfs -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/dfs/ipfs.rst"')
+os.system('wget  -P encryption -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/encryption/cryptor-unencrypted.rst"')
 os.system('wget  -P blockchain -N "https://raw.githubusercontent.com/evannetwork/dbcp/develop/docs/blockchain/account-store.rst"')
 
 
