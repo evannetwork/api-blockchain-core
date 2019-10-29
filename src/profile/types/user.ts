2ed9b1cb88031554a0122c0f92119ac812c2708b
Tobias Winkler <tobias.winkler@evan.team>
2019-10-29 15:31:12 +0100

    fix user account type property
    - [CORE-604]

--- src/profile/types/user.ts
+++ src/profile/types/user.ts
@@ -69,12 +69,15 @@ export default JSON.parse(`{
         "type": "entry"
       },
       "type": {
+        "dataSchema": {
+          "type": "string"
+        },
         "permissions": {
           "0": [
             "set"
           ]
         },
-        "type": "string"
+        "type": "entry"
       }
     },
     "type": "metadata"
