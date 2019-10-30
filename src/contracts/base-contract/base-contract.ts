cf8c4e03ba3178b91d75ca1f19bf0b557d92f24c
Omair Liaquat Ali <omair.ali@evan.team>
2019-10-30 11:19:35 +0100

    added check for invalid bc domain

--- src/contracts/base-contract/base-contract.ts
+++ src/contracts/base-contract/base-contract.ts
@@ -113,6 +113,18 @@ export class BaseContract extends Logger {
     let businessCenterAddress;
     if (businessCenterDomain) {
       businessCenterAddress = await this.options.nameResolver.getAddress(businessCenterDomain);
+      if (businessCenterAddress != '0x0000000000000000000000000000000000000000') {
+        const businessCenterContract = await this.options.loader.loadContract('BusinessCenterInterface', businessCenterAddress);
+        //console.log(businessCenterContract)
+        try{
+          const returnedContract = await this.options.executor.executeContractCall(businessCenterContract,'joinSchema');
+          console.log(returnedContract)
+        }
+        catch(e){
+          console.log(e)
+          throw new Error("There is no Business Center domain exisiting");
+        }
+      }
     } else {
       businessCenterAddress = '0x0000000000000000000000000000000000000000';
     }
