afb900c539cd7306cf50794d79e3f406b54a48a2
wulfraem <wulfraem@users.noreply.github.com>
2019-11-26 15:22:46 +0100

    update src/contracts/signer-identity.spec.ts
    - [CORE-631]
    
    Co-Authored-By: Sebastian Dechant <sebastian.dechant@evan.team>

--- src/contracts/signer-identity.spec.ts
+++ src/contracts/signer-identity.spec.ts
@@ -347,7 +347,7 @@ describe('signer-identity (identity based signer)', function() {
             to: contract.options.address,
             gas: 100e3,
             value: amountToSend,
-          })).to.rejected;
+          })).to.be.rejected;
 
           const balanceAfter = new BigNumber(await web3.eth.getBalance(signer.activeIdentity));
           expect(balanceAfter.eq(balanceBefore)).to.be.true;
