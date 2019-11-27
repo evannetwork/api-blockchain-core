29811225dc90957193e10e85388a159b51f324d2
Tschuck <tobias.winkler@evan.team>
2019-11-27 07:16:49 +0100

    Update src/contracts/digital-twin/container.ts
    Co-Authored-By: wulfraem <wulfraem@users.noreply.github.com>

--- src/contracts/digital-twin/container.ts
+++ src/contracts/digital-twin/container.ts
@@ -800,7 +800,7 @@ export class Container extends Logger {
   /**
    * Takes a full share configuration for a accountId (or a list of them), share newly added
    * properties and unshare removed properties from the container. Also accepts a list / instance of
-   * the original sharing configurations duplicated loading can be avoided.
+   * the original sharing configurations, so that duplicate loading can be avoided.
    *
    * @param      {ContainerShareConfig[]}  newConfigs       sharing configurations that should be
    *                                                        persisted
