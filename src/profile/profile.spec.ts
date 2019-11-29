be55d82a9613b0e2b201ea7b2e090e45423a3fa7
Omair Ali <36410182+OmairLiaquatAli@users.noreply.github.com>
2019-11-29 16:14:50 +0100

    Update src/profile/profile.spec.ts
    -[CORE-713]
    
    Co-Authored-By: wulfraem <wulfraem@users.noreply.github.com>

--- src/profile/profile.spec.ts
+++ src/profile/profile.spec.ts
@@ -442,7 +442,7 @@ describe('Profile helper', function() {
       await Onboarding.createNewProfile(initRuntime, newMnemonic, 'Test1234', {
         accountDetails: {
           profileType: 'user',
-          accountName: 'test account'
+          accountName: 'test account',
         }
       });
       const runtime = await getProfileRuntime(newMnemonic);
