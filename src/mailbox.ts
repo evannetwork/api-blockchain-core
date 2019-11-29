b25f710c163c48beaa90d6d0c22258bb62ca5707
wulfraem <wulfraem@users.noreply.github.com>
2019-11-29 14:39:12 +0100

    refactor code
    - function, property sorting
    - ';'s in interfaces instead of ','
    - add visibility modifier
    - [CORE-602]

--- src/mailbox.ts
+++ src/mailbox.ts
@@ -25,33 +25,35 @@ import {
   KeyProvider,
 } from '@evan.network/dbcp';
 
-import { CryptoProvider } from './encryption/crypto-provider';
-import { Ipld } from './dfs/ipld';
-import { Ipfs } from './dfs/ipfs';
+import {
+  CryptoProvider,
+  Ipld,
+  Ipfs,
+} from './index';
 
 /**
  * mail object
  */
 export interface Mail {
   content: {
-    attachments?: any[],
-    body?: string,
-    from?: string,
-    fromAlias?: string,
-    fromMail?: string,
-    sent?: number,
-    title?: string,
-    to?: string,
-  },
-  answers?: MailboxResult,
-  parentId?: string,
+    attachments?: any[];
+    body?: string;
+    from?: string;
+    fromAlias?: string;
+    fromMail?: string;
+    sent?: number;
+    title?: string;
+    to?: string;
+  };
+  answers?: MailboxResult;
+  parentId?: string;
 }
 
 /**
  * collection of mails
  */
 export interface MailboxResult {
-  mails: { [index: string]: Mail },
+  mails: { [index: string]: Mail };
   totalResultCount: number;
 }
 
@@ -59,13 +61,13 @@ export interface MailboxResult {
  * parameters for Mailbox constructor
  */
 export interface MailboxOptions extends LoggerOptions {
-  mailboxOwner: string;
-  nameResolver: NameResolver;
-  ipfs: Ipfs;
   contractLoader: ContractLoader;
   cryptoProvider: CryptoProvider;
-  keyProvider: KeyProvider;
   defaultCryptoAlgo: string;
+  ipfs: Ipfs;
+  keyProvider: KeyProvider;
+  mailboxOwner: string;
+  nameResolver: NameResolver;
 }
 
 /**
@@ -74,152 +76,25 @@ export interface MailboxOptions extends LoggerOptions {
  * @class      Mailbox (name)
  */
 export class Mailbox extends Logger {
-  mailboxOwner: string;
-  nameResolver: NameResolver;
-  contractLoader: ContractLoader;
-  mailboxContract: any;
-  ipfs: Ipfs;
-  cryptoProvider: CryptoProvider;
-  keyProvider: KeyProvider;
-  defaultCryptoAlgo: string;
-  initialized: boolean;
+  public contractLoader: ContractLoader;
+  public cryptoProvider: CryptoProvider;
+  public defaultCryptoAlgo: string;
+  public initialized: boolean;
+  public ipfs: Ipfs;
+  public keyProvider: KeyProvider;
+  public mailboxContract: any;
+  public mailboxOwner: string;
+  public nameResolver: NameResolver;
 
-  constructor(options: MailboxOptions) {
+  public constructor(options: MailboxOptions) {
     super(options);
-    this.mailboxOwner = options.mailboxOwner;
-    this.nameResolver = options.nameResolver;
     this.contractLoader = options.contractLoader;
-    this.ipfs = options.ipfs;
-    this.keyProvider = options.keyProvider;
     this.cryptoProvider = options.cryptoProvider;
     this.defaultCryptoAlgo = options.defaultCryptoAlgo;
-  }
-
-  /**
-   * initialize mailbox module
-   *
-   * @return     {Promise<void>}  resolved when done
-   */
-  async init(): Promise<void> {
-    if (this.initialized) {
-      return;
-    } else {
-      const domain = this.nameResolver.getDomainName(this.nameResolver.config.domains.mailbox);
-      const address = await this.nameResolver.getAddress(domain);
-      this.mailboxContract = this.contractLoader.loadContract('MailBoxInterface', address);
-      this.initialized = true;
-    }
-  }
-
-  async getSentMails(count = 10, offset = 0) {
-    return await this.getMails(count, offset, 'Sent');
-  }
-
-  /**
-   * gets received from mailboxOwner
-   *
-   * @param      {number}                  count   number of mails to retrieve (default 10)
-   * @param      {number}                  offset  mail offset (default 0)
-   * @return     {Promise<MailboxResult>}  The received mails.
-   */
-  async getReceivedMails(count = 10, offset = 0) {
-    return await this.getMails(count, offset, 'Received');
-  }
-
-  /**
-   * Gets the last n mails, resolved contents
-   *
-   * @param      {number}        count   retrieve up to this many answers (for paging)
-   * @param      {number}        offset  skip this many answers (for paging)
-   * @param      {string}        type    retrieve sent or received mails
-   * @return     {Promise<any>}  resolved mails
-   */
-  async getMails(count = 10, offset = 0, type = 'Received'): Promise<MailboxResult> {
-    await this.init();
-    const results: MailboxResult = {
-      mails: {},
-      totalResultCount: 0,
-    };
-
-    const executor = this.nameResolver.executor;
-    const listAddressHash = await executor.executeContractCall(
-      this.mailboxContract, `getMy${type}Mails`, { from: this.mailboxOwner, });
-    if (listAddressHash !== '0x0000000000000000000000000000000000000000000000000000000000000000') {
-      const listAddress = this.nameResolver.bytes32ToAddress(listAddressHash);
-      const listContract = this.contractLoader.loadContract('DataStoreList', listAddress);
-      const listLength = await executor.executeContractCall(listContract, 'length');
-      results.totalResultCount = parseInt(listLength.toString(), 10);
-      if (results.totalResultCount) {
-        let ipld: Ipld;
-        const mailIds = await this.nameResolver.getArrayFromListContract(listContract, count, offset, true);
-        const originator = this.nameResolver.soliditySha3.apply(this.nameResolver, [
-          this.nameResolver.soliditySha3(this.mailboxOwner), this.nameResolver.soliditySha3(this.mailboxOwner), ].sort());
-        ipld = new Ipld({
-          ipfs: this.ipfs,
-          keyProvider: this.keyProvider,
-          cryptoProvider: this.cryptoProvider,
-          defaultCryptoAlgo: this.defaultCryptoAlgo,
-          originator,
-          nameResolver: this.nameResolver,
-        });
-        for (let mailId of mailIds) {
-          try {
-            const mailResult = await executor.executeContractCall(this.mailboxContract, 'getMail', mailId);
-            const mail = await ipld.getLinkedGraph(mailResult.data);
-            const hashedSender = this.nameResolver.soliditySha3(mail.content.from);
-            if (hashedSender !== mailResult.sender) {
-              throw new Error(`mail claims to be sent from ${hashedSender}, but was sent from ${mailResult.sender}`);
-            } else {
-              results.mails[mailId] = mail;
-            }
-          } catch (ex) {
-            this.log(`could not unpack mail: "${mailId}"; ${ex.message || ex}`, 'warning');
-            results.mails[mailId] = null;
-          }
-        }
-      }
-    }
-    return results;
-  }
-
-  /**
-   * Gets one single mail directly
-   *
-   * @param      {string}         mail  mail to resolve (mailId or hash)
-   * @return     {Promise<Mail>}  The mail.
-   */
-  async getMail(mail: string): Promise<Mail> {
-    await this.init();
-    const executor = this.nameResolver.executor;
-    let ipld: Ipld;
-    const originator = this.nameResolver.soliditySha3.apply(this.nameResolver, [
-      this.nameResolver.soliditySha3(this.mailboxOwner), this.nameResolver.soliditySha3(this.mailboxOwner), ].sort());
-    ipld = new Ipld({
-      ipfs: this.ipfs,
-      keyProvider: this.keyProvider,
-      cryptoProvider: this.cryptoProvider,
-      defaultCryptoAlgo: this.defaultCryptoAlgo,
-      originator,
-      nameResolver: this.nameResolver,
-    });
-
-    try {
-      if (mail.startsWith('Qm')) {
-        return await ipld.getLinkedGraph(mail);
-      } else {
-        const mailResult = await executor.executeContractCall(this.mailboxContract, 'getMail', mail);
-        const mailItem = await ipld.getLinkedGraph(mailResult.data);
-        const hashedSender = this.nameResolver.soliditySha3(mailItem.content.from);
-        if (hashedSender !== mailResult.sender) {
-          throw new Error(`mail claims to be sent from ${hashedSender}, but was sent from ${mailResult.sender}`);
-        } else {
-          return mailItem;
-        }
-      }
-    } catch (ex) {
-      this.log(`could not decrypt mail: "${mail}"; ${ex.message || ex}`, 'warning');
-      return null;
-    }
+    this.ipfs = options.ipfs;
+    this.keyProvider = options.keyProvider;
+    this.mailboxOwner = options.mailboxOwner;
+    this.nameResolver = options.nameResolver;
   }
 
   /**
@@ -230,7 +105,7 @@ export class Mailbox extends Logger {
    * @param      {number}         offset  skip this many answers (for paging)
    * @return     {Promise<any>}  answer tree for mail
    */
-  async getAnswersForMail(mailId: string, count = 5, offset = 0): Promise<any> {
+  public async getAnswersForMail(mailId: string, count = 5, offset = 0): Promise<any> {
     await this.init();
     const results: MailboxResult = {
       mails: {},
@@ -280,39 +155,148 @@ export class Mailbox extends Logger {
   }
 
   /**
-   * sends a mail to given target
+   * returns amount of UTC deposited for a mail
    *
-   * @param      {Mail}           mail     a mail to send
-   * @param      {string}         from     account id to send mail from
-   * @param      {string}         to       account id to send mail to
-   * @param      {string}         value    (optional) UTC amount to send with mail in Wei can be
-   *                                       created with web3[.utils].toWei(...)
-   * @param      {string}         context  encrypt mail with different context
-   * @return     {Promise<void>}  resolved when done
+   * @param      {string}           mailId  mail to resolve
+   * @return     {Promise<string>}  balance of the mail in Wei  can be converted with
+   *                                web3[.utils].fromWei(...)
    */
-  async sendMail(mail: Mail, from: string, to: string, value = '0', context?: string): Promise<void> {
+  public async getBalanceFromMail(mailId: string): Promise<string> {
     await this.init();
-    mail.content.from = from;
-    mail.content.sent = new Date().getTime();
-    mail.content.to = to;
-    const combinedHash = this.nameResolver.soliditySha3.apply(this.nameResolver,
-      [this.nameResolver.soliditySha3(to), this.nameResolver.soliditySha3(this.mailboxOwner)].sort());
-    const ipld: Ipld = new Ipld({
+    // mailboxOwner
+    return this.nameResolver.executor.executeContractCall(
+      this.mailboxContract,
+      'getBalanceFromMail',
+      mailId,
+      { from: this.mailboxOwner, },
+    );
+  }
+
+  /**
+   * Gets one single mail directly
+   *
+   * @param      {string}         mail  mail to resolve (mailId or hash)
+   * @return     {Promise<Mail>}  The mail.
+   */
+  public async getMail(mail: string): Promise<Mail> {
+    await this.init();
+    const executor = this.nameResolver.executor;
+    let ipld: Ipld;
+    const originator = this.nameResolver.soliditySha3.apply(this.nameResolver, [
+      this.nameResolver.soliditySha3(this.mailboxOwner), this.nameResolver.soliditySha3(this.mailboxOwner), ].sort());
+    ipld = new Ipld({
       ipfs: this.ipfs,
       keyProvider: this.keyProvider,
       cryptoProvider: this.cryptoProvider,
-      originator: context ? this.nameResolver.soliditySha3(context) : combinedHash,
       defaultCryptoAlgo: this.defaultCryptoAlgo,
+      originator,
       nameResolver: this.nameResolver,
     });
-    const hash = await ipld.store(mail);
-    await this.nameResolver.executor.executeContractTransaction(
-      this.mailboxContract,
-      'sendMail',
-      { from, autoGas: 1.1, value, },
-      [ to ],
-      hash,
-    );
+
+    try {
+      if (mail.startsWith('Qm')) {
+        return await ipld.getLinkedGraph(mail);
+      } else {
+        const mailResult = await executor.executeContractCall(this.mailboxContract, 'getMail', mail);
+        const mailItem = await ipld.getLinkedGraph(mailResult.data);
+        const hashedSender = this.nameResolver.soliditySha3(mailItem.content.from);
+        if (hashedSender !== mailResult.sender) {
+          throw new Error(`mail claims to be sent from ${hashedSender}, but was sent from ${mailResult.sender}`);
+        } else {
+          return mailItem;
+        }
+      }
+    } catch (ex) {
+      this.log(`could not decrypt mail: "${mail}"; ${ex.message || ex}`, 'warning');
+      return null;
+    }
+  }
+
+  /**
+   * Gets the last n mails, resolved contents
+   *
+   * @param      {number}        count   retrieve up to this many answers (for paging)
+   * @param      {number}        offset  skip this many answers (for paging)
+   * @param      {string}        type    retrieve sent or received mails
+   * @return     {Promise<any>}  resolved mails
+   */
+  public async getMails(count = 10, offset = 0, type = 'Received'): Promise<MailboxResult> {
+    await this.init();
+    const results: MailboxResult = {
+      mails: {},
+      totalResultCount: 0,
+    };
+
+    const executor = this.nameResolver.executor;
+    const listAddressHash = await executor.executeContractCall(
+      this.mailboxContract, `getMy${type}Mails`, { from: this.mailboxOwner, });
+    if (listAddressHash !== '0x0000000000000000000000000000000000000000000000000000000000000000') {
+      const listAddress = this.nameResolver.bytes32ToAddress(listAddressHash);
+      const listContract = this.contractLoader.loadContract('DataStoreList', listAddress);
+      const listLength = await executor.executeContractCall(listContract, 'length');
+      results.totalResultCount = parseInt(listLength.toString(), 10);
+      if (results.totalResultCount) {
+        let ipld: Ipld;
+        const mailIds = await this.nameResolver.getArrayFromListContract(listContract, count, offset, true);
+        const originator = this.nameResolver.soliditySha3.apply(this.nameResolver, [
+          this.nameResolver.soliditySha3(this.mailboxOwner), this.nameResolver.soliditySha3(this.mailboxOwner), ].sort());
+        ipld = new Ipld({
+          ipfs: this.ipfs,
+          keyProvider: this.keyProvider,
+          cryptoProvider: this.cryptoProvider,
+          defaultCryptoAlgo: this.defaultCryptoAlgo,
+          originator,
+          nameResolver: this.nameResolver,
+        });
+        for (let mailId of mailIds) {
+          try {
+            const mailResult = await executor.executeContractCall(this.mailboxContract, 'getMail', mailId);
+            const mail = await ipld.getLinkedGraph(mailResult.data);
+            const hashedSender = this.nameResolver.soliditySha3(mail.content.from);
+            if (hashedSender !== mailResult.sender) {
+              throw new Error(`mail claims to be sent from ${hashedSender}, but was sent from ${mailResult.sender}`);
+            } else {
+              results.mails[mailId] = mail;
+            }
+          } catch (ex) {
+            this.log(`could not unpack mail: "${mailId}"; ${ex.message || ex}`, 'warning');
+            results.mails[mailId] = null;
+          }
+        }
+      }
+    }
+    return results;
+  }
+
+  /**
+   * gets received from mailboxOwner
+   *
+   * @param      {number}                  count   number of mails to retrieve (default 10)
+   * @param      {number}                  offset  mail offset (default 0)
+   * @return     {Promise<MailboxResult>}  The received mails.
+   */
+  public async getReceivedMails(count = 10, offset = 0) {
+    return await this.getMails(count, offset, 'Received');
+  }
+
+  public async getSentMails(count = 10, offset = 0) {
+    return await this.getMails(count, offset, 'Sent');
+  }
+
+  /**
+   * initialize mailbox module
+   *
+   * @return     {Promise<void>}  resolved when done
+   */
+  public async init(): Promise<void> {
+    if (this.initialized) {
+      return;
+    } else {
+      const domain = this.nameResolver.getDomainName(this.nameResolver.config.domains.mailbox);
+      const address = await this.nameResolver.getAddress(domain);
+      this.mailboxContract = this.contractLoader.loadContract('MailBoxInterface', address);
+      this.initialized = true;
+    }
   }
 
   /**
@@ -325,7 +309,7 @@ export class Mailbox extends Logger {
    *                                       created with web3[.utils].toWei(...)
    * @return     {Promise<void>}  resolved when done
    */
-  async sendAnswer(mail: Mail, from: string, to: string, value = '0'): Promise<void> {
+  public async sendAnswer(mail: Mail, from: string, to: string, value = '0'): Promise<void> {
     await this.init();
     mail.content.sent = new Date().getTime();
     const combinedHash = this.nameResolver.soliditySha3.apply(this.nameResolver,
@@ -351,20 +335,38 @@ export class Mailbox extends Logger {
   }
 
   /**
-   * returns amount of UTC deposited for a mail
+   * sends a mail to given target
    *
-   * @param      {string}           mailId  mail to resolve
-   * @return     {Promise<string>}  balance of the mail in Wei  can be converted with
-   *                                web3[.utils].fromWei(...)
+   * @param      {Mail}           mail     a mail to send
+   * @param      {string}         from     account id to send mail from
+   * @param      {string}         to       account id to send mail to
+   * @param      {string}         value    (optional) UTC amount to send with mail in Wei can be
+   *                                       created with web3[.utils].toWei(...)
+   * @param      {string}         context  encrypt mail with different context
+   * @return     {Promise<void>}  resolved when done
    */
-  async getBalanceFromMail(mailId: string): Promise<string> {
+  public async sendMail(mail: Mail, from: string, to: string, value = '0', context?: string): Promise<void> {
     await this.init();
-    // mailboxOwner
-    return this.nameResolver.executor.executeContractCall(
+    mail.content.from = from;
+    mail.content.sent = new Date().getTime();
+    mail.content.to = to;
+    const combinedHash = this.nameResolver.soliditySha3.apply(this.nameResolver,
+      [this.nameResolver.soliditySha3(to), this.nameResolver.soliditySha3(this.mailboxOwner)].sort());
+    const ipld: Ipld = new Ipld({
+      ipfs: this.ipfs,
+      keyProvider: this.keyProvider,
+      cryptoProvider: this.cryptoProvider,
+      originator: context ? this.nameResolver.soliditySha3(context) : combinedHash,
+      defaultCryptoAlgo: this.defaultCryptoAlgo,
+      nameResolver: this.nameResolver,
+    });
+    const hash = await ipld.store(mail);
+    await this.nameResolver.executor.executeContractTransaction(
       this.mailboxContract,
-      'getBalanceFromMail',
-      mailId,
-      { from: this.mailboxOwner, },
+      'sendMail',
+      { from, autoGas: 1.1, value, },
+      [ to ],
+      hash,
     );
   }
 
@@ -375,7 +377,7 @@ export class Mailbox extends Logger {
    * @param      {string}         recipient  account, that receives the EVEs
    * @return     {Promise<void>}  resolved when done
    */
-  async withdrawFromMail(mailId: string, recipient: string): Promise<void> {
+  public async withdrawFromMail(mailId: string, recipient: string): Promise<void> {
     await this.init();
     // mailboxOwner
     await this.nameResolver.executor.executeContractTransaction(
