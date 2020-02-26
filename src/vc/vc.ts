33ebb284d1fb31550102e87d7b08fc5251ae4c43
OmairLiaquatAli <omair.ali@evan.team>
2020-02-26 15:28:46 +0100

    add check for fetching revoke status of non-existing Vc
    -[CORE-1062]

--- src/vc/vc.ts
+++ src/vc/vc.ts
@@ -262,14 +262,19 @@ export class Vc extends Logger {
    * @return     {revokationStatus}  A boolean value. False = not revoked, True = revoked
    */
   public async getRevokeVcStatus(vcId: string): Promise<void> {
-    const environment = await this.getEnvironment();
-    const vcIdHash = vcId.replace(`vc:evan:${environment}:`, '');
-    const revokationStatus = await this.options.executor.executeContractCall(
-      await this.getRegistryContract(),
-      'vcRevoke',
-      vcIdHash,
-    );
-
+    let revokationStatus;
+    const vcDocument = await this.getVc(vcId);
+    if (!vcDocument) {
+      throw new Error(`Given "${vcId}" is not a valid evan VC`);
+    } else {
+      const environment = await this.getEnvironment();
+      const vcIdHash = vcId.replace(`vc:evan:${environment}:`, '');
+      revokationStatus = await this.options.executor.executeContractCall(
+        await this.getRegistryContract(),
+        'vcRevoke',
+        vcIdHash,
+      );
+    }
     return revokationStatus;
   }
 
