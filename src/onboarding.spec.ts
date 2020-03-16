4d6b7d51313d3bc0a5d87ddfa5c4abf05cf19a94
Philip Kaiser <Philser@users.noreply.github.com>
2020-03-16 13:25:34 +0100

    Update src/onboarding.spec.ts
    Co-Authored-By: Tschuck <tobias.winkler@evan.team>

--- src/onboarding.spec.ts
+++ src/onboarding.spec.ts
@@ -76,7 +76,7 @@ describe('Onboarding helper', function test() {
     const password = 'Test1234';
     const mnemonicNew = Onboarding.createMnemonic();
     const runtimeConfig: any = await Onboarding.generateRuntimeConfig(mnemonicNew, password, web3);
-    const balance = await web3.eth.getBalance(runtime.activeAccount);
+    const balance = await web3.eth.getBalance(runtime.underlyingAccount);
     const minimumAmount = web3.utils.toWei('1.0097');
 
     expect(Number(balance)).to.be.gt(Number(minimumAmount));
