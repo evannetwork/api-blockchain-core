692665c0c4b683493c91dfbaeaa464bfa4976f60
Philip Kaiser <Philser@users.noreply.github.com>
2020-03-16 13:28:10 +0100

    Update src/verifications/verifications.spec.ts
    Co-Authored-By: Tschuck <tobias.winkler@evan.team>

--- src/verifications/verifications.spec.ts
+++ src/verifications/verifications.spec.ts
@@ -1675,7 +1675,7 @@ describe('Verifications handler', function test() {
       // on identity[2]s side
       const runtime2 = runtimes[2];
       for (const i of txInfos.keys()) {
-        const verificationId = await runtime2.verifications.executeVerification(
+        const verificationId = await runtimes[2].verifications.executeVerification(
           identity2, txInfos[i],
         );
         await timeout(1000);
